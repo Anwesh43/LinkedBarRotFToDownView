d469387a951ba79e81201306dbbe078b9098ce84
Anwesh43 <anweshthecool0@gmail.com>
2021-06-29 01:27:34 +0530

    addng BRDTNode class

--- barrottodownview/src/main/java/com/example/barrottodownview/BarRotDownToView.kt
+++ barrottodownview/src/main/java/com/example/barrottodownview/BarRotDownToView.kt
@@ -116,4 +116,45 @@ class BarRotDownToView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BRDTNode(var i : Int, val state : State = State()) {
+
+        private var next : BRDTNode? = null
+        private var prev : BRDTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BRDTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBRDTNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+             state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> BRDTNode) : BRDTNode {
+            var curr : BRDTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
