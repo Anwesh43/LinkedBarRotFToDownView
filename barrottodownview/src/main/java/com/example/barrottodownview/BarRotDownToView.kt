09e717d536388173b286c7e49eac5ee1a676956b
Anwesh43 <anweshthecool0@gmail.com>
2021-06-29 00:40:20 +0530

    adding constants and libraries

--- barrottodownview/src/main/java/com/example/barrottodownview/BarRotDownToView.kt
+++ barrottodownview/src/main/java/com/example/barrottodownview/BarRotDownToView.kt
@@ -1,1 +1,27 @@
 package com.example.barrottodownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#004D40",
+    "#FFD600",
+    "#00C853",
+    "#6200EA"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 2
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 8.9f
+val delay : Long = 20
+val deg : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+
